d92dcdad0f13ea89bf361ccf4f8dfb744aef6a47
Juan Jose Medina <71518079+juanchi1789@users.noreply.github.com>
2021-11-06 14:59:44 -0300

    Nuevas modificaciones en el codigo

--- testeo_juan_partes.ino
+++ testeo_juan_partes.ino
@@ -3,6 +3,7 @@ int IR1 = A1;
 int IR2 = A2;
 int IR3 = A3;
 int clave1 = 2;
+int clave2 = 4;
 int bonsai = 0;
 
 
@@ -12,30 +13,36 @@ void setup() {
   pinMode(IR2,INPUT);
   pinMode(IR3,INPUT);
   pinMode(clave1,OUTPUT);
+  pinMode(clave2,OUTPUT);
   pinMode(bonsai,OUTPUT);
   Serial.begin(9600);
   }
 
 void loop() {
-  String valores = "";
+  
+
+  
+  // La entrada
   float value0 = (5. / 1023) * analogRead(IR0); // Pos
   float value1 = (5. / 1023) * analogRead(IR1); // Leng
+  float value2 = (5. / 1023) * analogRead(IR2); // Pos
+  float value3 = (5. / 1023) * analogRead(IR3); // Leng
+  
+  // Cargamos los valores
+  String valores = "";
   valores.concat(String(value0));
   valores.concat(" ");
   valores.concat(String(value1));
+  valores.concat(" ");
+  valores.concat(String(value2));
+  valores.concat(" ");
+  valores.concat(String(value3));
   
   Serial.println(valores);
-
- //Serial.print("Pos: "); Serial.print(value1); Serial.print("  ");
-  //Serial.println(",");
-  //bonsai = Serial.read();
   
-  //if(value0 > 3 && value1 > 3 && bonsai != -1){
-  if(value0 > 3 && bonsai != -1){
-    digitalWrite(clave1,HIGH);
-  }
-  else{
-    digitalWrite(clave1,LOW);
-  }
   
+  //bonsai = Serial.read(); ==> tenemos que agregar la funcion que interactua con el las bobinas
+  
+  
+    
 }
